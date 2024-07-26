08c555433dd68b61952374ab3461d89fdbb8e01f
Bruno0071 <bruno_santiago00@outlook.com>
2024-07-26 12:35:59 -0300

    Projeto frase do Dia (Array)

---
+++ array.js
@@ -0,0 +1,19 @@
+let date = new Date();
+let mes = date.getMonth();
+
+var mes_atual = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro"
+];
+
+console.log(mes_atual[mes]);
